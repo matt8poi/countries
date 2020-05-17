4ce39e07f27b51cb92e1a4622d7db1a09eb8d42e
matt8poi <matt8poi@gmail.com>
2020-05-17 10:07:18 -0600

    Initial setup.

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+            <form>
+                find countries <input />
+            </form>
+        </div>
+    )
+}
+
+
+export default Search
